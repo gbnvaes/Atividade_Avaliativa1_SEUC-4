15c2401a28797956ad4b001d7ee82bccd22b3b5b
leleo <leonardo.as7@puccampinas.edu.br>
2026-04-30 20:51:21 -0300

    Implementação da função metricas e revisão da mesma.

--- funcoes.py
+++ funcoes.py
@@ -24,3 +24,18 @@ def verificar_travamento(zona_atual, zona_anterior):
         return True
     return False
 
+def metricas_finais(total_pressao, menor_pressao, cont_verde, i, total_leituras, travamento):
+    if i == 0:
+        print("Nenhuma leitura realizada")
+        return
+    media = total_pressao / i
+    porc_verde = (cont_verde * 100) / i
+
+    print(f"Média das pressões ajustadas: {media: .2f} UPCs")
+    print(f"Menor pressão registrada: {menor_pressao: .2f} UPCs")
+    print(f"Porcentagem na Zona Verde: {porc_verde: .2f}%")
+    
+    if travamento:
+        porc_leituras = (i * 100 ) / total_leituras
+        print(f"Percentual de leituras realizadas: {porc_leituras: .2f}%")
+
