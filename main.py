b388c37e4677d875a7d6946905f1222131894b1e
leonardo <leonardo.as7@puccampinas.edu.br>
2026-05-04 11:55:24 -0300

    Validar que total_leituras seja maior que zero e Validação de pressão negativa ou zero

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from funcoes import *
 import os
-zona_atual = None
-total_leituras = int(input("Digite o número total de leituras do turno: "))
+zona_atual = None 
 total_pressao = 0
 menor_pressao = None
 maior_pressao = None
@@ -9,7 +8,12 @@ cont_verde = 0
 travamento = False
 i = 0
 opcao = 0
+total_leituras = 0 
 
+while total_leituras <= 0:
+    total_leituras = int(input("Digite o número total de leituras do turno: "))
+    if total_leituras <= 0:
+        print("O número de leituras deve ser maior que zero. Tente Novamente. ")
 
 while opcao !=1 and opcao!=2:
     os.system('cls')
@@ -17,9 +21,17 @@ while opcao !=1 and opcao!=2:
 
 for i in range(1,total_leituras + 1):
     if opcao == 1:
-        upc = int(input("Pressão: "))
+        upc = 0
+        while upc <= 0:
+            upc = int(input(f"Leitura {i}/{total_leituras} - Pressão (UPC): "))
+            if upc <= 0:
+                print("Pressão inválida. Digite um valor maior que zero.")
     else:
         upc = pressao()
+    if upc > 150:
+        upc *= 1.08
+    else:
+        upc *= 0.96
     total_pressao += upc
 
     if menor_pressao == None or upc < menor_pressao:
@@ -30,6 +42,8 @@ for i in range(1,total_leituras + 1):
     zona_anterior = zona_atual
     zona_atual = classificar_zona(upc)
 
+    print(f"  → Pressão ajustada: {upc:.2f} UPCs | Zona: {zona_atual}")
+
     if zona_atual == "Verde (Estável)":
         cont_verde += 1
         
@@ -38,7 +52,7 @@ for i in range(1,total_leituras + 1):
         travamento = True 
         print("O sistema atingiu o limite de zona vermelha\n")
         break
-
+print()
 metricas_finais(total_pressao, menor_pressao, maior_pressao, cont_verde, i, total_leituras, travamento)
 
 
